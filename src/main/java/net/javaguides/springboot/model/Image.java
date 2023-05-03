b6e4126f1abe3d3bb67b3279579fb620d4f8867b
Ludrnn-8 <ludrnn-8@student.ltu.se>
2023-05-03 10:05:46 +0200

    commit

---
+++ src/main/java/net/javaguides/springboot/model/Image.java
@@ -0,0 +1,77 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Image")
+public class Image {
+	
+	@Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+    
+    @Column(name = "tags")
+    private String tags;
+    
+    @Column(name = "categories")
+    private String category;
+
+    @Column(name = "picByte", length = 2000)
+	private byte[] picByte;
+    
+    //Timestamp should be added:
+    //private Date dateOfUpload;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+       this.description = description;
+    }
+    public byte[] getPicByte() {
+        return picByte;
+    }
+    public void setPicByte(byte[] picByte) {
+        this.picByte = picByte;
+    }
+	public String getTags() {
+		return tags;
+	}
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+    
+}
+
+
